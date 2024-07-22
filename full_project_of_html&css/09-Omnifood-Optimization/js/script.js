4508665ff699f3a79110b12a9e5f44c5e5dca84d
bemmGr <123631107+Bemmmgr@users.noreply.github.com>
2024-07-22 11:38:34 +0800

    add js code to style scrolling links

--- full_project_of_html&css/09-Omnifood-Optimization/js/script.js
+++ full_project_of_html&css/09-Omnifood-Optimization/js/script.js
@@ -1,9 +1,9 @@
-// console.log("hello, world!")
+console.log("hello, world!")
 
-// const myName = 'Xiangyu Li';
-// const h1 = document.querySelector(".heading-primiary")
-// console.log(myName);
-// console.log(h1);
+const myName = 'Xiangyu Li';
+const h1 = document.querySelector(".heading-primiary")
+console.log(myName);
+console.log(h1);
 
 // h1.addEventListener('click', function() {
 //   h1.textContent = myName;
@@ -11,9 +11,48 @@
 //   h1.style.padding = "5rem";
 // })
 
+///////////////////////////////////////////////////////////
+//set current year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
+//Make mobile navigation work
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
+btnNavEl.addEventListener('click', function() {
+  headerEl.classList.toggle("nav-open");
+})
+
+///////////////////////////////////////////////////////////
+//Smooth scrolling animation
+const allLinks = document.querySelectorAll('a:link');
+//console.log(allLinks);
+allLinks.forEach(function(link) {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+
+    //Scroll to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    })
+
+    //Scroll to other links
+    if (href !== "#" && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({behavior: "smooth"});
+    }
+
+    //Close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+
+  })
+})
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
